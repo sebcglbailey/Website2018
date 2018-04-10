84f5758a7b9c1ee50b64ab776821c025144f765f
Seb Bailey <sebastianbailey@sebastians-MacBook-Pro.local>
2018-04-10 23:42:45 +0100

    fix after broken images

--- personal-react-website/src/Components/ProjectImages/ProjectImages.js
+++ personal-react-website/src/Components/ProjectImages/ProjectImages.js
@@ -5,11 +5,36 @@ import styles from './ProjectImages.css';
 class ProjectImages extends Component {
 	constructor(props) {
 		super(props)
+
+		this.state = {
+			images: this.props.images
+		}
+
+		this.getImages = this.getImages.bind(this)
+	}
+
+	componentWillReceiveProps(nextState) {
+		if (nextState.images !== this.state.images) {
+			this.setState({ images: nextState.images })
+		}
+	}
+
+	getImages() {
+		let images = this.state.images.map((imgName) => {
+			let imgSrc = require(`../../Projects/${this.props.project}/img/${imgName}`)
+			return <img src={imgSrc} />
+		})
+		return images
 	}
 
 	render() {
+		let images = this.getImages()
 		return(
-			<div className={styles.images}></div>
+			<div className={styles.container}>
+				<div className={styles.images}>
+					{images}
+				</div>
+			</div>
 		)
 	}
 }
